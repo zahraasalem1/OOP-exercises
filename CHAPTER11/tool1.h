46632f2a4a48911f6031f9b556655543a9597406
zahraa <zahraa.salem.20077@gmail.com>
2026-07-12 04:02:35 +0300

    new file

---
+++ CHAPTER11/tool1.h
@@ -0,0 +1,10 @@
+namespace TOOL1
+{
+inline double calculate(double num1, double num2)
+{
+return num1+num2;
+}
+}
+#ifndef TOOL1_H_INCLUDED
+#define TOOL1_H_INCLUDED
+#endif
---
+++ CHAPTER11/tool2.h
@@ -0,0 +1,10 @@
+namespace TOOL2
+{
+inline double calculate(double num1, double num2)
+{
+    return num1*num2;
+}
+}
+#ifndef TOOL2_H_INCLUDED
+#define TOOL2_H_INCLUDED
+#endif
